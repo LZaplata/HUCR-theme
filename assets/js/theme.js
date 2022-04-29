3ce834ca93de0e5fb91fde0dd474ff169b53f743
zap <zap@hucr.cz>
2022-04-29 10:45:56 +0200

    theme initialization

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // headroom on header
 
-    var headroom  = new Headroom(document.querySelector("header"));
+    var headroom = new Headroom(document.querySelector("header"));
 
     headroom.init();
 
@@ -21,4 +21,28 @@ $(document).ready(function () {
     $(".offcanvas .nav li a").click(function () {
         $(".offcanvas button").click();
     });
+
+
+    // claim animation
+
+    function wordsAnimate() {
+        $("#claim .words span").delay(2000).animate({
+            borderSpacing: -100
+        }, {
+            duration: 1000,
+            step: function (now, fx) {
+                $(this).css("-webkit-transform", "translateY(" + now + "%)");
+            }
+        }).promise().done(function () {
+            var wordsSpan = $("#claim .words span");
+
+            wordsSpan.first().clone().appendTo("#claim .words");
+            wordsSpan.first().remove();
+            wordsSpan.css("-webkit-transform", "translateY(0)");
+
+            wordsAnimate();
+        });
+    }
+
+    wordsAnimate();
 });
